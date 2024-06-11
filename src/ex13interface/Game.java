98c47f87be5f765489be292ad3ea6d1b426ebf0c
cutetrick <tngkrdms8@gmail.com>
2024-06-11 16:06:56 +0900

    20240611 수업완료

--- src/ex13interface/Game.java
+++ src/ex13interface/Game.java
@@ -5,5 +5,5 @@ package ex13interface;
   하나의 Java 파일에 하나의 public클래스 혹은 인터페이스를 선언하는것을
   권장한다. */
 public interface Game {
-
+	int SCISSORS=1 ,ROCK=2, PAPER=3;
 }
---
+++ src/ex13interface/figure/IDrawable.java
@@ -0,0 +1,5 @@
+package ex13interface.figure;
+// '그리다' 행위를 표현한 인터페이스
+public interface IDrawable {
+	void draw(String figureName);
+}
